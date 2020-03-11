2192550f11ab73d04df6c49f67f49500d396986e
Michael Hough <michaelh@uk.ibm.com>
2020-03-11 14:08:19 +0000

    Load add-zsh-hook function
    Previously I've been loading prompt-tweaks alongside other themes, so the kubectl prompt has worked. On my VSCode, it fails with: command not found: add-zsh-hook
    
    Loading the add-zsh-hook function guarantees that we can use it.

--- prompt-tweaks.zsh
+++ prompt-tweaks.zsh
@@ -1,6 +1,7 @@
 # When combined with molepigeon.zsh-theme, the prompt shows your targeted Kube cluster, and highlights it in blue if the cluster name contains "-stage", red if it contains "-prod".
 # IKS cluster configs set the context name to <clusterName>/<clusterID>, so the context is split on slash to remove the cluster ID and save space.
 
+autoload -Uz add-zsh-hook
 add-zsh-hook precmd _zsh_molepigeon_prompt_precmd
 function _zsh_molepigeon_prompt_precmd() {
     context=$(kubectl config current-context | cut -d/ -f1)
